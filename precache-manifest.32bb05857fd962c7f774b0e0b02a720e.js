ddfa1f17052e7bb32bcb08682a91a78c4ff02265
Alvin Mantovani <alvin.ictn@gmail.com>
2020-09-17 00:19:21 +0700

    Updates

---
+++ precache-manifest.32bb05857fd962c7f774b0e0b02a720e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5ed534f782d47853e35c6cedc19d18",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "3f4e1d6962ec1466b6e9",
+    "url": "/todo-react/static/css/main.d9847bb1.chunk.css"
+  },
+  {
+    "revision": "581584dd1999ed91cb9d",
+    "url": "/todo-react/static/js/2.8eb0dc2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.8eb0dc2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4e1d6962ec1466b6e9",
+    "url": "/todo-react/static/js/main.33028f65.chunk.js"
+  },
+  {
+    "revision": "0fb193e52e48fff8eee7",
+    "url": "/todo-react/static/js/runtime-main.d3cc96ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
